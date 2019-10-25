374301879c2c6a242f81e9965891853bb38b4caa
ZhengFeijie <zhengfeijie@xunlei.com>
2019-10-25 23:21:25 +0800

    update rollup

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
-import { terser } from "rollup-plugin-terser"
 import babelrc from 'babelrc-rollup'
 import commonjs from 'rollup-plugin-commonjs'
+import { terser } from "rollup-plugin-terser"
 
 const config = {
   input: 'src/vue-observer-directive.js',
